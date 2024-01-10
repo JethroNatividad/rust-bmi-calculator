0d4cdcdeeabc460aafd169dd8728ade37a2c0042
JethroNatividad <natividad.jet@gmail.com>
2024-01-10 21:29:04 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 // Write a program that calculates the BMI
-
+// Inputs: height inches, weight lbs.
+// Process: bmi = (weight / (height × height))* 703
+// Output: if bmi < 18.5, underweight, > 25 overweight, in between is normal.
+    // Your BMI is {}.
+    // "You are underweight. You should see your doctor." | "You are within the ideal weight range." | "You are overweight. You should see your doctor."
 
 fn main() {
     println!("Hello, world!");
